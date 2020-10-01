7791c58df40e71b38a0b96e209f0a69ec665e289
rune <rune.krogh.leevre@gmail.com>
2020-10-01 11:47:28 +0200

    123

--- netværksspil/src/game/ClientRecieveThread.java
+++ netværksspil/src/game/ClientRecieveThread.java
@@ -4,25 +4,24 @@ import java.io.BufferedReader;
 import java.net.Socket;
 import java.util.ArrayList;
 
-
 public class ClientRecieveThread extends Thread {
-	
+
 	private Socket theSocket;
 	private BufferedReader inFromServer;
 	private Player player;
-	
+
 	public ClientRecieveThread(Socket theSocket, BufferedReader inFromServer, Player player) {
 		this.theSocket = theSocket;
 		this.inFromServer = inFromServer;
 		this.player = player;
 	}
-	
+
 	public void run() {
-		while(theSocket.isConnected()) {
+		while (theSocket.isConnected()) {
 			recieveData();
 		}
 	}
-	
+
 	public void recieveData() {
 		try {
 			String recived = inFromServer.readLine();
@@ -31,13 +30,13 @@ public class ClientRecieveThread extends Thread {
 			System.out.println(e);
 		}
 	}
-	
-	public void updatePlayer(int xpos, int ypos,  String direction) {
+
+	public void updatePlayer(int xpos, int ypos, String direction) {
 		player.direction = direction;
 		player.xpos = xpos;
 		player.ypos = ypos;
 	}
-	
+
 	public void updatePlayerName(String pName) {
 		player.name = pName;
 	}
