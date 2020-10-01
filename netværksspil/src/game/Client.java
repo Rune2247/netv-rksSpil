1e92ff7c483378f0e060d65ed8123995808761fa
Andreas Bech Holm <Andreas Bech Holm@10.24.67.112>
2020-10-01 14:59:28 +0200

    Score

--- netværksspil/src/game/Client.java
+++ netværksspil/src/game/Client.java
@@ -121,60 +121,13 @@ public class Client extends Application {
 			fields[p.getX()][p.getY()].setGraphic(new ImageView(hero_up));
 			*/
 
-			scoreList.setText(getScoreList());
+			updateScoreTable();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public pair getRandomFreePosition()
-	// finds a random new position which is not wall
-	// and not occupied by other players
-	{
-		int x = 1;
-		int y = 1;
-		boolean found = false;
-		while (!found) {
-			Random r = new Random();
-			x = Math.abs(r.nextInt() % 18) + 1;
-			y = Math.abs(r.nextInt() % 18) + 1;
-			if (Generel.board[y].charAt(x) == ' ') {
-				found = true;
-				for (Player p : players) {
-					if (p.xpos == x && p.ypos == y)
-						found = false;
-				}
-
-			}
-		}
-		pair p = new pair(x, y);
-		return p;
-	}
-
-	public void movePlayerOnScreen(int oldx, int oldy, int newx, int newy, String direction) {
-		Platform.runLater(() -> {
-			fields[oldx][oldy].setGraphic(new ImageView(image_floor));
-		});
-		Platform.runLater(() -> {
-			if (direction.equals("right")) {
-				fields[newx][newy].setGraphic(new ImageView(hero_right));
-			}
-			;
-			if (direction.equals("left")) {
-				fields[newx][newy].setGraphic(new ImageView(hero_left));
-			}
-			;
-			if (direction.equals("up")) {
-				fields[newx][newy].setGraphic(new ImageView(hero_up));
-			}
-			;
-			if (direction.equals("down")) {
-				fields[newx][newy].setGraphic(new ImageView(hero_down));
-			}
-			;
-		});
-
-	}
+	
 
 	public static void updateAllPlayers() {
 		System.out.println(players.size());
@@ -205,36 +158,10 @@ public class Client extends Application {
 		});
 	}
 
-	public void updatePlayer(int delta_x, int delta_y, String direction) {
-		me.direction = direction;
-		int x = me.getXpos(), y = me.getYpos();
-
-		if (Generel.board[y + delta_y].charAt(x + delta_x) == 'w') {
-			me.addPoints(-1);
-		} else {
-			// prepared for collision detection
-			// not quite relevant in single plaver version
-			Player p = getPlayerAt(x + delta_x, y + delta_y);
-			if (p != null) {
-				me.addPoints(10);
-				// update the other player
-				p.addPoints(-10);
-				pair pa = getRandomFreePosition();
-				p.xpos = pa.getX();
-				p.ypos = pa.getY();
-				movePlayerOnScreen(x + delta_x, y + delta_y, pa.getX(), pa.getY(), p.direction);
-			} else
-				me.addPoints(1);
-			movePlayerOnScreen(x, y, x + delta_x, y + delta_y, direction);
-			me.setXpos(x + delta_x);
-			me.setYpos(y + delta_y);
-		}
-
-	}
 
 	public void updateScoreTable() {
 		Platform.runLater(() -> {
-			scoreList.setText(getScoreList());
+			scoreList.setText("Score: " + Client.score);
 		});
 	}
 
@@ -248,17 +175,6 @@ public class Client extends Application {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		//updatePlayer(delta_x, delta_y, direction);
-		//updateScoreTable();
-	}
-
-	public String getScoreList() {
-		StringBuffer b = new StringBuffer(100);
-		for (Player p : players) {
-			b.append(p + "\r\n");
-		}
-		return b.toString();
 	}
 
 	public Player getPlayerAt(int x, int y) {
@@ -289,8 +205,6 @@ public class Client extends Application {
 		ClientRecieveThread recieveThread = new ClientRecieveThread(clientSocket, inFromServer);
 		recieveThread.start();
 
-		Client.outToServer.writeBytes("Hej Rune" + '\n');
-		Client.outToServer.writeBytes("Nissr" + '\n');
 		
 		launch(args);
 
