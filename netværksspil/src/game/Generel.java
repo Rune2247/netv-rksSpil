93d41912657923f5fa538d4ac3afd52765c70a7a
rune <rune.krogh.leevre@gmail.com>
2020-10-08 11:18:29 +0200

    Map

--- netværksspil/src/game/Generel.java
+++ netværksspil/src/game/Generel.java
@@ -1,29 +1,28 @@
 package game;
 
 public class Generel {
-	public static  String[] board = {    // 20x20
-			"wwwwwwwwwwwwwwwwwwww",
-			"w                  w",
-			"w                  w",
-			"w                  w",
-			"w                  w",
-			"w                  w",
-			"w                  w",
-			"w                  w",
-			"w                  w",
-			"w        12        w",
-			"w        34        w",
-			"w                  w",
-			"w                  w",
-			"w                  w",
-			"w                  w",
-			"w  p p             w",
-			"w   b              w",
-			"w   b              w",
-			"w   b              w",
-			"wwwwwwwwwwwwwwwwwwww"
-	};
-
 	
 
+	  public static String[] board = { // 20x20 
+	  "wwwwwwwwwwwwwwwwwwww",
+	  "w         w ww     w",
+	  "w wwwwwwww     www w",
+	  "w  w   ww  w www   w",
+	  "ww w w  www  ww  www",
+	  "w  w www  w wwwww  w", 
+	  "w ww     w  bb  ww w",
+	  "w  w w www ww w w  w",
+	  "ww ww  w       b w w",
+	  "w wwwwww 12 w w w  w",
+	  "w   p    34 ww   w w", 
+	  "wwww w w    w ww   w",
+	  "w   ww ww www   ww w",
+	  "w w     w pp  w w  w",
+	  "w   www www w w w ww", 
+	  "ww wwww   w w   w  w",
+	  "w   wwwww w  wwwww w",
+	  "w wwwww   w   www  w", 
+	  "w   w   w wwwwwww  w",
+	  "wwwwwwwwwwwwwwwwwwww" };
+	 
 }
